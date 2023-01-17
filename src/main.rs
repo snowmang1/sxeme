67d80b9099c97f604f992d5fb5aecd0bcf8c553a
Evan Drake <evanmdrake@icloud.com>
2023-01-17 18:34:33 -0500

    added parser & scanner dirs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*
+ * This is the sxeme Compiler src This is to be the main or *driver* file
+ * the parsers dir houses all parser content
+ */
+mod parsers;
+mod scanner;
+
 fn main() {
-    println!("Hello, Scheme r7rs");
+    parsers::help();
 }
---
+++ src/parsers/mod.rs
@@ -0,0 +1,4 @@
+
+pub fn help() -> () {
+    println!("hello from parsers")
+}
