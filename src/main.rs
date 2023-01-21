6a737505cb205ec27414386bfa30aae06fc6576b
Evan Drake <evanmdrake@icloud.com>
2023-01-21 13:56:52 -0500

    scanner takes a File struct as input and gives back empty string

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
  * This is the sxeme Compiler src This is to be the main or *driver* file
  * the parsers dir houses all parser content
  */
+#![feature(buf_read_has_data_left)]
+#![feature(file_create_new)]
+
 mod parsers;
 mod scanner;
 
--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,4 +1,49 @@
-
 /* 
  * scanner related interface
+ * scanners will be implemented to read text in and format it into words/lines/symbols
+ * & then give that data to the parser.
+ *
+ * SHOULD NOT BE THROWING ERRORS HERE.....EVER
+ * 
+ * NOTE
+ * as this file gets bigger add files to this module
 */
+use std::fs::File;
+use std::io::{ BufRead, BufReader };
+
+#[allow(dead_code)] /* temporary */
+fn get_prog(buf: &mut BufReader<File>) -> Result<Vec<String>, std::io::Error>{
+    let mut prog: Vec<String> = vec![];
+    match buf.has_data_left() {
+        Ok(false) => Ok(vec![]),
+        Err(x)    => Err(x),
+        Ok(true)  => {
+            for line in buf.lines() { /* loop to itterate on each character of BufReader */
+                prog.push(line.unwrap());
+            }
+            Ok(prog)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn get_prog_open_file_test() {
+        let f = match File::create_new("foo.txt"){ // create_new opens in read-write mode
+            Ok(ok)   => ok,
+            Err(err) => panic!("{err}"),
+        };
+        let result: Vec<String> = match get_prog(&mut BufReader::new(f)) {
+            Ok(ok)   => ok,
+            Err(err) => panic!("{err}"),
+        };
+        let expected: Vec<String> = vec![];
+        assert_eq!(result, expected);
+        match std::fs::remove_file("foo.txt") {
+            Ok(ok)   => ok,
+            Err(err) => panic!("{err}")
+        };
+    }
+}
