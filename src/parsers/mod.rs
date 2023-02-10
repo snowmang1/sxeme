c7fd7372f2754ec1b7b17a79280e96fed0acdd81
Evan Drake <evanmdrake@icloud.com>
2023-02-10 09:35:48 -0800

    emptying prog & filling TokenStack
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -3,11 +3,68 @@ mod tests;
 
 use crate::scanner;
 
+// TODO
+// move types to own dir
+// move parsers to own file
+
+#[derive(Default, Debug, PartialEq, Clone)]
+struct TokenStack { stack: Vec<(String, u32)>, position: u32, open: u32 }
+
 #[allow(dead_code)]
-fn parse_driver(prog_name: String) {
-    let _prog: Vec<String> = match scanner::get_prog(&prog_name) {
+impl TokenStack {
+    fn init(pos: u32, st: Vec<(String, u32)>, o: u32) -> Self { TokenStack { position: pos, stack: st, open: o } }
+    fn print(&self) { println!("start:"); for (s,_) in self.stack.clone() { println!("{s}"); } }
+    // above is DEBUG use only
+    fn push(&mut self, lex: String) -> Result<u32, String>{
+        match lex.as_str() {
+            "(" => { // state 1
+                self.stack.push((lex, self.position));
+                self.open+=1;
+                Ok(self.position)
+            }
+            ")" => { // state 5 reduce
+                self.stack.push((lex, self.position));
+                self.position=0; self.open-=1;
+                Ok(self.position)
+            }
+            "+" => { // state 2 operation
+                self.stack.push((lex, self.position));
+                self.position+=2;
+                Ok(self.position)
+            }
+            "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => { // state 3
+                self.stack.push((lex, self.position));
+                Ok(self.position)
+            }
+            x   => Err(format!("unknown symbol {x}"))
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn parse_driver(prog_name: String) -> Result<(), &'static str>{
+    let mut stack: TokenStack = Default::default();
+    let mut prog: Vec<String> = match scanner::get_prog(&prog_name) {
         Ok(ok)   => ok,
-        Err(err) => panic!("{err}")
+        Err(err) => panic!("{err}") // panic is passing io::error error up
     };
 
+    // send to parsers
+    match arithmatic_parser(&mut prog, &mut stack) {
+        Err(err) => panic!("arithmatic Error: {err}"),
+        Ok(_)   => Ok(())
+    }
+}
+
+#[allow(dead_code)]
+fn arithmatic_parser(prog: &mut Vec<String>, stack: &mut TokenStack) -> Result<(), String> {
+    let mut pos: u32 = 0;
+    let mut ers: Vec<String> = Default::default();
+    while !prog.is_empty() {
+        match stack.push(prog.remove(pos as usize)) { // Beware
+            Ok(ok)   => {pos=ok; println!("{ok}")}
+            Err(err) => ers.push(err)
+        }
+    }
+    Ok(())
 }
--- src/parsers/tests/mod.rs
+++ src/parsers/tests/mod.rs
@@ -1,16 +1,31 @@
 use super::*;
 
-#[test]
-fn test_empty_prog() {
-    let mut test_prog: Vec<String> = vec![];
-    let result = arithmatic_parser(&mut test_prog, &mut vec![]);
-    assert_eq!(result, Err("empty file"))
+fn mk_svec(v: Vec<&str>) -> Vec<String> {
+    let mut new: Vec<String> = Default::default();
+    for s in v {
+        new.push(String::from(s));
+    }
+    new
 }
 
 #[test]
-fn test_addition_no_nesting() {
-    let mut test_prog = vec![String::from("("), String::from("+"), String::from("1"), String::from("2"), String::from(")")];
-    let result = arithmatic_parser(&mut test_prog, &mut vec![]);
-    let expected_stack = vec![String::from("(+12)")];
-    assert_eq!( result, Ok((vec![], expected_stack)) )
+fn test_arith_empty() {
+    let mut test_prog: Vec<String> = mk_svec(vec![]);
+    let mut tok: TokenStack = Default::default();
+    let expected_prog: Vec<String> = vec![];
+    let result = arithmatic_parser(&mut test_prog, &mut tok);
+    assert_eq!(result, Ok(()));
+    assert_eq!(test_prog, expected_prog);
+}
+
+#[test]
+fn test_arith_reduce_1() {
+    let mut test_prog: Vec<String> = mk_svec(vec!["(", "+", "1", "2", ")"]);
+    let mut tok: TokenStack = Default::default();
+    let expected_prog: Vec<String> = vec![];
+    let result = arithmatic_parser(&mut test_prog, &mut tok);
+    assert_eq!(result, Ok(()));
+    assert_eq!(test_prog, expected_prog);
+    let (top,_) = tok.stack[0].to_owned();
+    assert_eq!(top, String::from("(+12)"))
 }
