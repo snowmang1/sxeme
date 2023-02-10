3d695d17dc47fea4947e605d2f0116364de0a38a
Evan Drake <evanmdrake@icloud.com>
2023-02-10 12:37:10 -0800

    basic parser working, no errors given on bad code
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -8,33 +8,40 @@ use crate::scanner;
 // move parsers to own file
 
 #[derive(Default, Debug, PartialEq, Clone)]
-struct TokenStack { stack: Vec<(String, u32)>, position: u32, open: u32 }
+struct TokenStack { stack: Vec<String>, open: u32 }
 
 #[allow(dead_code)]
 impl TokenStack {
-    fn init(pos: u32, st: Vec<(String, u32)>, o: u32) -> Self { TokenStack { position: pos, stack: st, open: o } }
-    fn print(&self) { println!("start:"); for (s,_) in self.stack.clone() { println!("{s}"); } }
+    fn init(st: Vec<String>, o: u32) -> Self { TokenStack { stack: st, open: o } }
     // above is DEBUG use only
-    fn push(&mut self, lex: String) -> Result<u32, String>{
+    fn push(&mut self, lex: String) -> Result<(), String>{
         match lex.as_str() {
             "(" => { // state 1
-                self.stack.push((lex, self.position));
+                self.stack.push(lex);
                 self.open+=1;
-                Ok(self.position)
+                Ok(())
             }
             ")" => { // state 5 reduce
-                self.stack.push((lex, self.position));
-                self.position=0; self.open-=1;
-                Ok(self.position)
+                let mut accum = String::from(")");
+                loop {
+                    match self.stack.pop() {
+                        Some(s) => { match s.as_str() {
+                            "(" => { accum.insert(0, '('); self.stack.push(accum); break },
+                            _   => () // Errors
+                        }; accum.insert_str(0, s.as_str()); }
+                        None    => () // Errors
+                    }
+                }
+                self.open-=1;
+                Ok(())
             }
             "+" => { // state 2 operation
-                self.stack.push((lex, self.position));
-                self.position+=2;
-                Ok(self.position)
+                self.stack.push(lex);
+                Ok(())
             }
             "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => { // state 3
-                self.stack.push((lex, self.position));
-                Ok(self.position)
+                self.stack.push(lex);
+                Ok(())
             }
             x   => Err(format!("unknown symbol {x}"))
         }
@@ -58,13 +65,12 @@ fn parse_driver(prog_name: String) -> Result<(), &'static str>{
 
 #[allow(dead_code)]
 fn arithmatic_parser(prog: &mut Vec<String>, stack: &mut TokenStack) -> Result<(), String> {
-    let mut pos: u32 = 0;
     let mut ers: Vec<String> = Default::default();
     while !prog.is_empty() {
-        match stack.push(prog.remove(pos as usize)) { // Beware
-            Ok(ok)   => {pos=ok; println!("{ok}")}
+        match stack.push(prog.remove(0 as usize)) {
+            Ok(_)    => (),
             Err(err) => ers.push(err)
-        }
+        };
     }
     Ok(())
 }
--- src/parsers/tests/mod.rs
+++ src/parsers/tests/mod.rs
@@ -19,6 +19,17 @@ fn test_arith_empty() {
 }
 
 #[test]
+fn test_arith_drain() {
+    let mut test_prog: Vec<String> = mk_svec(vec!["(", "+", "1", "1", ")"]);
+    let mut tok: TokenStack = Default::default();
+    let expected_prog: Vec<String> = vec![];
+    let result = arithmatic_parser(&mut test_prog, &mut tok);
+    assert_eq!(result, Ok(()));
+    assert_eq!(test_prog, expected_prog);
+    assert!(tok.stack.len() != 0);
+}
+
+#[test]
 fn test_arith_reduce_1() {
     let mut test_prog: Vec<String> = mk_svec(vec!["(", "+", "1", "2", ")"]);
     let mut tok: TokenStack = Default::default();
@@ -26,6 +37,23 @@ fn test_arith_reduce_1() {
     let result = arithmatic_parser(&mut test_prog, &mut tok);
     assert_eq!(result, Ok(()));
     assert_eq!(test_prog, expected_prog);
-    let (top,_) = tok.stack[0].to_owned();
+    let top = tok.stack[0].to_owned();
     assert_eq!(top, String::from("(+12)"))
+}
+
+#[test]
+fn test_arith_ignore_extra_paren() {
+    let mut test_prog: Vec<String> = mk_svec(vec!["(", "(", "+", "1", "2", ")"]);
+    let mut tok: TokenStack = Default::default();
+    arithmatic_parser(&mut test_prog, &mut tok).unwrap();
+    assert_eq!("(", tok.stack[0].as_str());
+    assert_eq!("(+12)", tok.stack[1].as_str());
+}
+
+#[test]
+fn test_arith_no_closing_paren_error() {
+    let mut test_prog: Vec<String> = mk_svec(vec!["(", "(", "+", "1", "2"]);
+    let mut tok: TokenStack = Default::default();
+    let result = arithmatic_parser(&mut test_prog, &mut tok).unwrap();
+
 }
